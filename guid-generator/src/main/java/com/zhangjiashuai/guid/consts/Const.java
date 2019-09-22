288f8f5e988343915849a3aa392b2bcbb535d67d
jiashuaizhang <33648447+jiashuaizhang@users.noreply.github.com>
2019-09-21 23:25:18 -0500

    Update Const.java

--- guid-generator/src/main/java/com/zhangjiashuai/guid/consts/Const.java
+++ guid-generator/src/main/java/com/zhangjiashuai/guid/consts/Const.java
@@ -9,10 +9,9 @@ public class Const {
 	public static final String ROOT = "guid";
 	public static final String DEFAULT_TABLE = "_default_";
 	
-	private static Random machineIdRandom = new Random();
-	private static Random dataCenterIdRandom = new Random(); 
+	private static Random idRandom = new Random();
 	
-	public static final long DEFAULT_DATACENTER_ID = machineIdRandom.nextInt((int) SnowFlake.MAX_DATACENTER_NUM);
-	public static final long DEFAULT_MACHINEID = dataCenterIdRandom.nextInt((int) SnowFlake.MAX_MACHINE_NUM);;
+	public static final long DEFAULT_DATACENTER_ID = idRandom.nextInt((int) SnowFlake.MAX_DATACENTER_NUM);
+	public static final long DEFAULT_MACHINEID = idRandom.nextInt((int) SnowFlake.MAX_MACHINE_NUM);
 	
 }

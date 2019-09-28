04e13b11d45fe51d2e9db1fb0c5e6e5898984485
jiashuaizhang@126.com <jiashuaizhang@126.com>
2019-09-28 09:59:13 +0800

    客户端自动配置

---
+++ guid-generator-zookeeper-client-spring-boot-starter/src/main/java/com/zhangjiashuai/guid/client/config/GuidGeneratorZooKeeperClientAutoConfiguration.java
@@ -0,0 +1,11 @@
+package com.zhangjiashuai.guid.client.config;
+
+import org.springframework.context.annotation.Configuration;
+
+import com.zhangjiashuai.guid.client.annotation.EnableGuidGeneratorZooKeeperClient;
+
+@Configuration
+@EnableGuidGeneratorZooKeeperClient
+public class GuidGeneratorZooKeeperClientAutoConfiguration {
+
+}
